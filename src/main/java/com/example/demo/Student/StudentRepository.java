55b33d021be921f964240da5aa9c21c3dc19dd4c
Kevin Manavalan <kevinseby20@gmail.com>
2024-03-09 12:35:52 +0530

    code formatting

--- src/main/java/com/example/demo/Student/StudentRepository.java
+++ src/main/java/com/example/demo/Student/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+//@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -10,11 +10,10 @@ import java.util.Optional;
 
 @Service
 public class StudentService {
-
     private final StudentRepository studentRepository;
-
     @Autowired
     public StudentService(StudentRepository studentRepository){
+
         this.studentRepository = studentRepository;
     }
     public List<Student> getStudent() {
